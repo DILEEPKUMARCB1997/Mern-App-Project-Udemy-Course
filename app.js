2eb1467bcaf1a36d58ab1146e1be48c8855f980c
Dileep Kumar C B <dileep.k@atop.com.tw>
2023-04-17 14:20:58 +0530

    Added urlencoded

--- app.js
+++ app.js
@@ -11,21 +11,22 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyparser.json()); //-->for POST request
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use(express.static(path.join("public")));
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-//   next();
-// });
+  next();
+});
 
 app.use("/api/places", placesRoutes); //=> /api/places/...
 
